cb5fb94dec0e373c79ba177c70b6c5489fb41ad5
shm_usuf <shmusuf@gmail.com>
2023-10-08 13:42:43 +0600

    pages and routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -7,7 +7,7 @@ import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
@@ -15,7 +15,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
